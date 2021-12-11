86449dbb7c194a5e0e0537419d47b58168392953
Justin Holsclaw <jholsclaw@live.com>
2021-12-10 20:51:25 -0700

    made the test pass

--- web/src/App.test.js
+++ web/src/App.test.js
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import React from 'react-dom';
 test('renders learn react link', () => {
-  const SimpleComponent = () => <p>learn</p>
+  const SimpleComponent = () => <p>learn react</p>
   render(<SimpleComponent />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
