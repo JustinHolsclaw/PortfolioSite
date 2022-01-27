622afb0c38e9325d4f5833d700b1b33b2b33d2fd
Justin Holsclaw <justin.holsclaw@microfocus.com>
2022-01-26 20:49:33 -0700

    Added the form.

---
+++ wgadmin/src/wgadminPages/AdminHome.js
@@ -0,0 +1,37 @@
+function AdminHome() {
+
+    return (
+        <>
+            <form
+                id="admin-form"
+                >
+                <div className="form-group mb-3">
+                    <label htmlFor="clientname"><strong>Client Name:</strong></label>
+                    <input type="text"></input>
+                </div>
+                <div className="form-group mb-3">
+                    <label htmlFor="ipaddress"><strong>IP Address:</strong></label>
+                    <input type="text"></input>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="dateadded"><strong>Date Added:</strong></label>
+                    <input></input>
+                </div>
+                <div className="form-group mb-3">
+                    <label htmlFor="allowediprange"><strong>Allowed IP Range:</strong></label>
+                    <input></input>
+                </div>
+                <div className="form-group mb-3">
+                    <label><strong>Client Public Key:</strong></label>
+                    <input></input>
+                </div>
+                <div className="form-group mb-3">
+                    <label><strong>Client Private Key:</strong></label>
+                    <input></input>
+                </div>
+            </form>
+        </>
+    )
+}
+
+export default AdminHome;
