2eb388b38ba33e2112c365e056c70b23e1920ac6
Justin Holsclaw <justin.holsclaw@microfocus.com>
2022-02-27 21:29:16 -0700

    added routing to the new pages

--- wgadmin/src/App.js
+++ wgadmin/src/App.js
@@ -1,10 +1,16 @@
+import { Routes } from 'react-router-dom';
 import './App.css';
 import AdminHome from './wgadminPages/AdminHome';
+import NewUser from './wgadminPages/NewUser';
 
 function App() {
   return (
     <div className="App">
-      <AdminHome/>
+      <Routes>
+        <Route path="/" element={<AdminHome/>}/>
+        <Route path="/NewUser" element={<NewUser/>}/>
+      </Routes>
+      
     </div>
   );
 }
