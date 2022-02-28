737fe04f92daa9ff1581ce8b192015879f4dcd51
Justin Holsclaw <justin.holsclaw@microfocus.com>
2022-02-27 21:58:26 -0700

    removed Router

--- web/src/MainSwitch.js
+++ web/src/MainSwitch.js
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./Views/Home";
 import Project from "./Views/Project";
 import ContactMe from "./Views/ContactMe";
@@ -6,15 +6,13 @@ import Login from "./Components/Login/Login";
 
 const MainSwitch = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/projects" element={<Project/>}/>
-        <Route path="/resume" />
-        <Route path="/ContactMe" element={<ContactMe/>}/>
-        <Route path="/Login" element={<Login/>}/>
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/projects" element={<Project />} />
+      <Route path="/resume" />
+      <Route path="/ContactMe" element={<ContactMe />} />
+      <Route path="/Login" element={<Login />} />
+    </Routes>
   );
 };
 
