9309e7a38250d42c14d8e933062c151f117d086f
Justin Holsclaw <justin.holsclaw@microfocus.com>
2022-02-28 22:07:02 -0700

    changed import

--- web/src/Components/Secure/Secure.jsx
+++ web/src/Components/Secure/Secure.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import axios, {AxiosResponse, AxiosError} from "react";
+import axios, {AxiosResponse, AxiosError} from "axios";
 import {useNavigate} from 'react-router-dom';
 function Secure() {
     const navigate = useNavigate()
